1389529a44e2cf1aa7c8f59ff7cd2d1d02aa4cef
Manwish <53825622+Manwish@users.noreply.github.com>
2023-05-07 09:23:15 +1200

    Add files via upload

--- js.js
+++ js.js
@@ -1,87 +1,52 @@
 function attack(){
-	a=Math.floor(Math.random() * 34) + 1;
-	if (document.getElementById('oper').style.display!="none") document.getElementById('oper').style.display="none"; 
-	else document.getElementById(b).style.display="none"; 
-	switch(a){
-		case 1:	document.getElementById('a1').style.display="block"; break;
-		case 2:	document.getElementById('a2').style.display="block"; break;
-		case 3: document.getElementById('a3').style.display="block"; break;
-		case 4:	document.getElementById('a4').style.display="block"; break;
-		case 5:	document.getElementById('a5').style.display="block"; break;
-		case 6:	document.getElementById('a6').style.display="block"; break;
-		case 7: document.getElementById('a7').style.display="block"; break;
-		case 8:	document.getElementById('a8').style.display="block"; break;
-		case 9:	document.getElementById('a9').style.display="block"; break;
-		case 10: document.getElementById('a10').style.display="block"; break;
-		case 11: document.getElementById('a11').style.display="block"; break;
-		case 12: document.getElementById('a12').style.display="block"; break;
-		case 13: document.getElementById('a13').style.display="block"; break;
-		case 14: document.getElementById('a14').style.display="block"; break;
-		case 15: document.getElementById('a15').style.display="block"; break;
-		case 16: document.getElementById('a16').style.display="block"; break;
-		case 17: document.getElementById('a17').style.display="block"; break;
-		case 18: document.getElementById('a18').style.display="block"; break;
-		case 19: document.getElementById('a19').style.display="block"; break;
-		case 20: document.getElementById('a20').style.display="block"; break;
-		case 21: document.getElementById('a21').style.display="block"; break;
-		case 22: document.getElementById('a22').style.display="block"; break;
-		case 23: document.getElementById('a23').style.display="block"; break;
-		case 24: document.getElementById('a24').style.display="block"; break;
-		case 25: document.getElementById('a25').style.display="block"; break;
-		case 26: document.getElementById('a26').style.display="block"; break;
-		case 27: document.getElementById('a27').style.display="block"; break;
-		case 28: document.getElementById('a28').style.display="block"; break;
-		case 29: document.getElementById('a29').style.display="block"; break;
-		case 30: document.getElementById('a30').style.display="block"; break;
-		case 31: document.getElementById('a31').style.display="block"; break;
-		case 32: document.getElementById('a32').style.display="block"; break;
-		case 33: document.getElementById('a33').style.display="block"; break;
-		case 34: document.getElementById('a34').style.display="block"; break;	
-	}
-	b='a'+a;
-	return (a);
+	randomOper(1, 34, 'a');
 }
 
 function protect(){
-	a=Math.floor(Math.random() * 33) + 1;
+	randomOper(1, 33, 'b');
+}
+
+function randomOper(min, max, c){
+a=Math.floor(Math.random() * max) + min;
 	if (document.getElementById('oper').style.display!="none") document.getElementById('oper').style.display="none"; 
 	else document.getElementById(b).style.display="none"; 
 	switch(a){
-		case 1:	document.getElementById('b1').style.display="block"; break;
-		case 2:	document.getElementById('b2').style.display="block"; break;
-		case 3: document.getElementById('b3').style.display="block"; break;
-		case 4:	document.getElementById('b4').style.display="block"; break;
-		case 5:	document.getElementById('b5').style.display="block"; break;
-		case 6:	document.getElementById('b6').style.display="block"; break;
-		case 7: document.getElementById('b7').style.display="block"; break;
-		case 8:	document.getElementById('b8').style.display="block"; break;
-		case 9:	document.getElementById('b9').style.display="block"; break;
-		case 10: document.getElementById('b10').style.display="block"; break;
-		case 11: document.getElementById('b11').style.display="block"; break;
-		case 12: document.getElementById('b12').style.display="block"; break;
-		case 13: document.getElementById('b13').style.display="block"; break;
-		case 14: document.getElementById('b14').style.display="block"; break;
-		case 15: document.getElementById('b15').style.display="block"; break;
-		case 16: document.getElementById('b16').style.display="block"; break;
-		case 17: document.getElementById('b17').style.display="block"; break;
-		case 18: document.getElementById('b18').style.display="block"; break;
-		case 19: document.getElementById('b19').style.display="block"; break;
-		case 20: document.getElementById('b20').style.display="block"; break;
-		case 21: document.getElementById('b21').style.display="block"; break;
-		case 22: document.getElementById('b22').style.display="block"; break;
-		case 23: document.getElementById('b23').style.display="block"; break;
-		case 24: document.getElementById('b24').style.display="block"; break;
-		case 25: document.getElementById('b25').style.display="block"; break;
-		case 26: document.getElementById('b26').style.display="block"; break;
-		case 27: document.getElementById('b27').style.display="block"; break;
-		case 28: document.getElementById('b28').style.display="block"; break;
-		case 29: document.getElementById('b29').style.display="block"; break;
-		case 30: document.getElementById('b30').style.display="block"; break;
-		case 31: document.getElementById('b31').style.display="block"; break;
-		case 32: document.getElementById('b32').style.display="block"; break;
-		case 33: document.getElementById('b33').style.display="block"; break;
-		//case 34: document.getElementById('b34').style.display="block"; break;	
+		case 1:	document.getElementById(c+'1').style.display="block"; break;
+		case 2:	document.getElementById(c+'2').style.display="block"; break;
+		case 3: document.getElementById(c+'3').style.display="block"; break;
+		case 4:	document.getElementById(c+'4').style.display="block"; break;
+		case 5:	document.getElementById(c+'5').style.display="block"; break;
+		case 6:	document.getElementById(c+'6').style.display="block"; break;
+		case 7: document.getElementById(c+'7').style.display="block"; break;
+		case 8:	document.getElementById(c+'8').style.display="block"; break;
+		case 9:	document.getElementById(c+'9').style.display="block"; break;
+		case 10: document.getElementById(c+'10').style.display="block"; break;
+		case 11: document.getElementById(c+'11').style.display="block"; break;
+		case 12: document.getElementById(c+'12').style.display="block"; break;
+		case 13: document.getElementById(c+'13').style.display="block"; break;
+		case 14: document.getElementById(c+'14').style.display="block"; break;
+		case 15: document.getElementById(c+'15').style.display="block"; break;
+		case 16: document.getElementById(c+'16').style.display="block"; break;
+		case 17: document.getElementById(c+'17').style.display="block"; break;
+		case 18: document.getElementById(c+'18').style.display="block"; break;
+		case 19: document.getElementById(c+'19').style.display="block"; break;
+		case 20: document.getElementById(c+'20').style.display="block"; break;
+		case 21: document.getElementById(c+'21').style.display="block"; break;
+		case 22: document.getElementById(c+'22').style.display="block"; break;
+		case 23: document.getElementById(c+'23').style.display="block"; break;
+		case 24: document.getElementById(c+'24').style.display="block"; break;
+		case 25: document.getElementById(c+'25').style.display="block"; break;
+		case 26: document.getElementById(c+'26').style.display="block"; break;
+		case 27: document.getElementById(c+'27').style.display="block"; break;
+		case 28: document.getElementById(c+'28').style.display="block"; break;
+		case 29: document.getElementById(c+'29').style.display="block"; break;
+		case 30: document.getElementById(c+'30').style.display="block"; break;
+		case 31: document.getElementById(c+'31').style.display="block"; break;
+		case 32: document.getElementById(c+'32').style.display="block"; break;
+		case 33: document.getElementById(c+'33').style.display="block"; break;
+		case 34: document.getElementById(c+'34').style.display="block"; break;	
 	}
-	b='b'+a;
+	b= c+a;
 	return (a);
+
 }
